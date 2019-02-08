25fec4423ea8993b88feb1bfcc4162ed37a14e36
PJusue <pablojusue@gmail.com>
2019-02-08 11:14:29 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-USER='bot'
-PASSWORD='P8Bz43XxF1Ea7au94fY4r1uVi2WjWbGHz17n9NL8tvBP82t81ERwM2a5nPNnVCj8'
-DATABASE='ig_users'
-HOST='127.0.0.1'
-TOKEN='715477854:AAFr2e7al9j50k8eehsQF6rE70gX5t2AIl8'
-URL = 'http://api.telegram.org/bot{0}/getUpdates'.format(TOKEN)
-PATH = 'C:\\Users\\pablo\\Desktop\\'
